db7702b20b7528a0b082ee2eab9441cf1b330dac
Blake Lockley <bwlockley@gmail.com>
2019-06-08 14:07:54 +1000

    New compile method using JSON

--- compile.py
+++ compile.py
@@ -1,32 +1,30 @@
 import os
+import json
 from jinja2 import Template
 from typing import List, Dict, Any
 
-
+TEMPLATE_DIR = "templates"
+RESUME_TEMPLATE = "index.j2.html"
 RESUME_DIR = "resume"
 OUT_FILE = "index.html"
 
 def main(args: List[str]):
 
-    resume: Dict[str, Dict[str, Any]] = {}
+    resume: Dict[str, List[Dict[str, str]]] = {}
 
     for section in os.listdir(RESUME_DIR):
-        print(section)
-        resume[section] = {}
-        for textfile in os.listdir(os.path.join(RESUME_DIR, section)):
-            print("--", textfile)
+        resume[section] = []
+        items = os.listdir(os.path.join(RESUME_DIR, section))
+        for textfile in sorted(items):
             fpath = os.path.join(RESUME_DIR, section, textfile)
             with open(fpath) as f:
-                name, _ = os.path.splitext(textfile)
-                resume[section][name] = f.read()
+                resume[section].append(json.load(f))
 
-    with open(os.path.join("templates", "index.j2.html")) as f:
+    with open(os.path.join(TEMPLATE_DIR, RESUME_TEMPLATE)) as f:
         template = Template(f.read())
 
-    
     with open(OUT_FILE, "w") as f:
-        rendered = template.render(**resume)
-        f.write(rendered)
+        f.write(template.render(**resume))
 
 if __name__ == "__main__":
     import sys
